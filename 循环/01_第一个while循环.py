f4dee43f161d720de9eb2284289cfa00ca806b39
CyforSkadi <86471817+CyforSkadi@users.noreply.github.com>
2021-12-12 21:28:16 +0800

    Add files via upload

---
+++ 循环/01_第一个while循环.py
@@ -0,0 +1,12 @@
+# Python 基础学习
+# 蛋挞不能吃
+# 学习时间： 2021/12/12 0012 20:17
+
+# 打印 5 遍 `Hello Python`
+i = 0
+
+while i < 5:
+
+    print("hello Python第%d次" % (i+1))
+
+    i += 1
---
+++ 循环/04_break语句.py
@@ -0,0 +1,15 @@
+# Python 基础学习
+# 蛋挞不能吃
+# 学习时间： 2021/12/12 0012 20:34
+
+i = 0
+
+while i <= 10:
+
+    if i == 3:
+        break
+    print(i)
+
+    i += 1
+
+print("over")
