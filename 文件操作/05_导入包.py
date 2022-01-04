97ddae0b488fc089c4c3a3c7a944b7d56f159450
CyforSkadi <86471817+CyforSkadi@users.noreply.github.com>
2022-01-04 14:39:27 +0800

    Delete 文件操作 directory

--- 文件操作/05_导入包.py
+++
@@ -1,9 +0,0 @@
-# Python 基础学习
-# 蛋挞不能吃
-# 学习时间： 2022/1/4 0004 13:40
-
-import message
-
-message.send_message.send("hello")
-text = message.receive_message.receive()
-print(text)
--- 文件操作/message/__init__.py
+++
@@ -1,7 +0,0 @@
-# Python 基础学习
-# 蛋挞不能吃
-# 学习时间： 2022/1/4 0004 13:40
-
-# 要在外界使用包的模块，需要在__init__模块中指定要导出的模块
-from . import send_message
-from . import receive_message
